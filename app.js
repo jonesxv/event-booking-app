8a75ffa791ae729f3291dcd1287434f5fcab825e
Ryan Jones <ryanjones33@gmail.com>
2019-03-21 11:06:51 -0700

    Added MongoDB connectivity

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+
+const Event = require('./models/event');
 
 const app = express();
-
-const events = [];
 
 app.use(bodyParser.json());
 
@@ -39,22 +40,41 @@ app.use(
     `),
     rootValue: {
       events: () => {
-        return events;
+        return Event.find()
+            .then(events => {
+                return events.map(event => {
+                    return {...event._doc, _id: event.id};
+                });
+            }).catch(err => {
+                throw err;
+            });
       },
       createEvent: args => {
-        const event = {
-          _id: Date.now(),
-          title: args.eventInput.title,
-          description: args.eventInput.description,
-          price: +args.eventInput.price,
-          date: args.eventInput.date
-        };
-        events.push(event);
-        return event;
+        const event = new Event({
+            title: args.eventInput.title,
+            description: args.eventInput.description,
+            price: +args.eventInput.price,
+            date: new Date(args.eventInput.date)
+        });
+        return event
+            .save()
+            .then(result => {
+                console.log(result);
+                return {...result._doc, _id: event.id};
+            }).catch(err => {
+                console.log(err);
+                return err;
+            });
       }
     },
     graphiql: true
   })
 );
 
-app.listen(3000);
+mongoose.connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-ug8li.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`
+    ).then(() => {
+        app.listen(3000);
+    }).catch(err => {
+        console.log(err);
+    })
